c5387f061a74ebd2b7cb8be33c329aa8f316772a
santhoshsridhar95 <47470408+santhoshsridhar95@users.noreply.github.com>
2025-07-15 01:54:02 +0530

    resolving error

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,11 @@
-import genres from "@/data/Genres";
-
+import genres from '../data/generes'
 export interface Genre{
     id: number;
     name: string;
     image_background: string;
 }
 
-const useGenres = () => ({data:genres, error: null, isLoading:false})
+const useGenres = () => ({data:genres as Genre[], error: null, isLoading:false})
     //  useData<Genre>('/genres');
 
 export default useGenres;
