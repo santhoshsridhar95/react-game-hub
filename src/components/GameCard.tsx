4cf95ff3e01f17eb48c7f7081d4162eb9f820734
santhoshsridhar95 <47470408+santhoshsridhar95@users.noreply.github.com>
2025-07-14 20:17:35 +0530

    Cleaning up the Game Card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,13 +1,5 @@
 import type { Game } from "@/hooks/useGames";
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  Heading,
-  HStack,
-  Image,
-  Text,
-} from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "@/services/image-url";
@@ -21,11 +13,11 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList platforms={game.platforms.map((p) => p.platform)} />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={3}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={4}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
