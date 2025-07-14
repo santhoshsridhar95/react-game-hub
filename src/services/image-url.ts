e75d0cabe48ee4e9a4bfdfd6cf49655dc7432b42
santhoshsridhar95 <47470408+santhoshsridhar95@users.noreply.github.com>
2025-07-14 18:41:06 +0530

    Handling games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
 
 const getCroppedImageUrl = (url:string) =>{
-    if(url === null) return;
+    if(url === null) return noImage;
     const target =  'media/';
     const index = url.indexOf(target) + target.length;
 
